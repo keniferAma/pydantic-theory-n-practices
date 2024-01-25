8b2de5da0c086e1d532a3aa8d81c8845fe41aa0e
kenifer <139505395+keniferAma@users.noreply.github.com>
2024-01-24 20:33:15 -0500

    Serialization: model_dump()_main_concepts.

---
+++ serialization/model_dump_concepts.py
@@ -0,0 +1,66 @@
+#### model_dump and its parameters #####
+# This is the main way that pyadantic uses to serialize data. model_dump gives us a dictionary result.
+# while model_dump_json gives us JSON-encoded structure (which aparently are the same)
+
+
+from typing import Any, List, Optional
+
+from pydantic import BaseModel, Field, Json
+
+
+class BarModel(BaseModel):
+    whatever: int
+
+
+class FooBarModel(BaseModel):
+    banana: Optional[float] = 1.1
+    foo: str = Field(serialization_alias='foo_alias')
+    bar: BarModel
+
+
+m = FooBarModel(banana=3.14, foo='hello', bar={'whatever': 123})
+
+# returns a dictionary:
+print(m.model_dump())
+#> {'banana': 3.14, 'foo': 'hello', 'bar': {'whatever': 123}}
+print(m.model_dump(include={'foo', 'bar'}))
+#> {'foo': 'hello', 'bar': {'whatever': 123}}
+print(m.model_dump(exclude={'foo', 'bar'})) 
+#> {'banana': 3.14}
+print(m.model_dump(by_alias=True))
+#> {'banana': 3.14, 'foo_alias': 'hello', 'bar': {'whatever': 123}}
+print(
+    FooBarModel(foo='hello', bar={'whatever': 123}).model_dump(
+        exclude_unset=True
+    )
+)
+#> {'foo': 'hello', 'bar': {'whatever': 123}}
+print(
+    FooBarModel(banana=1.1, foo='hello', bar={'whatever': 123}).model_dump(
+        exclude_defaults=True
+    )
+)
+#> {'foo': 'hello', 'bar': {'whatever': 123}}
+print(
+    FooBarModel(foo='hello', bar={'whatever': 123}).model_dump(
+        exclude_defaults=True
+    )
+)
+#> {'foo': 'hello', 'bar': {'whatever': 123}}
+print(
+    FooBarModel(banana=None, foo='hello', bar={'whatever': 123}).model_dump(
+        exclude_none=True
+    )
+)
+#> {'foo': 'hello', 'bar': {'whatever': 123}}
+
+
+class Model(BaseModel):
+    x: List[Json[Any]]
+
+
+print(Model(x=['{"a": 1}', '[1, 2]']).model_dump())
+#> {'x': [{'a': 1}, [1, 2]]}
+print(Model(x=['{"a": 1}', '[1, 2]']).model_dump(round_trip=True))
+#> {'x': ['{"a":1}', '[1,2]']}
+
